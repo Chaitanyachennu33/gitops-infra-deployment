85b249cfb8ddd18d3ceb1cafaa1c5bc7177d0fc8
Chaitanyachennu33 <116829295+Chaitanyachennu33@users.noreply.github.com>
2026-01-29 11:05:19 +0530

    Update main.tf

--- terrafomr/main.tf
+++ terrafomr/main.tf
@@ -1,8 +1,27 @@
-resource "aws_instance" "gitops_ec2" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "GitOps-EC2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
+  required_version = ">= 1.0"
+}
+ 
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+# Generate random suffix for unique bucket names
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+ 
+# Create S3 bucket (basic)
+resource "aws_s3_bucket" "example" {
+  bucket = "my-gitops-bucket-${random_id.suffix.hex}"
 }
